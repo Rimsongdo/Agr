2444e0a66f3189eca74370573ec3276f3ee01bdc
SAWADOGO Rimsomgdo Roger <rimssawadogo@gmail.com>
2024-12-01 21:30:35 +0100

    Premier commit

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  serialNumber: {
+    type: String,
+    required: false,
+    unique: true,
+    trim: true,
+  },
+  thingSpeakChannelId: {
+    type: String,
+    required: false,
+  },
+  thingSpeakApiKey: {
+    type: String,
+    required: false, 
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+},{
+  collection: 'Farmers' // Nom de la collection personnalisée
+});
+
+
+
+module.exports = mongoose.model('User', userSchema);
