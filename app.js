39a51c43d33ac910b5048f4a9403ac6e71da4294
SAWADOGO Rimsomgdo Roger <rimssawadogo@gmail.com>
2024-12-02 23:34:50 +0100

    Second C

--- app.js
+++ app.js
@@ -12,7 +12,30 @@ mongoose.connect(config.MONGODB_URL)
   .then(() => console.log('Connecté à MongoDB'))
   .catch((error) => console.error('Erreur de connexion à MongoDB:', error));
 
+const admin = require('./firebase'); // Importer Firebase Admin
 
+  // Fonction pour envoyer une notification
+  const sendNotification = async (fcmToken, title, body) => {
+    const message = {
+      token: fcmToken, // Token FCM de l'utilisateur cible
+      notification: {
+        title: title, // Titre de la notification
+        body: body,   // Corps de la notification
+      },
+      // Optionnel : Ajouter des données personnalisées
+      data: {
+        customKey: 'customValue',
+      },
+    };
+  
+    try {
+      const response = await admin.messaging().send(message);
+      console.log('Notification envoyée avec succès :', response);
+    } catch (error) {
+      console.error('Erreur lors de l\'envoi de la notification :', error);
+    }
+  };
+  
 app.use(cors());
 app.use(express.json());
 app.use('/api/userServices',userServices)
