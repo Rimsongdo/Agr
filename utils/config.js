275a34a6ef9369beedab4d2ff95fd6703462f829
SAWADOGO Rimsomgdo Roger <rimssawadogo@gmail.com>
2024-12-03 23:10:35 +0100

    f

--- utils/config.js
+++ utils/config.js
@@ -3,4 +3,5 @@ const PORT=5000
 //const MONGODB_URL="mongodb://0.0.0.0:27017/"
 const MONGODB_URL="mongodb+srv://Roger:0Rimsongdo0@reacttest.ntnjvtx.mongodb.net/?retryWrites=true&w=majority&appName=reactTest"
 const SECRET='ROGER'
+
 module.exports={PORT,MONGODB_URL,SECRET}
